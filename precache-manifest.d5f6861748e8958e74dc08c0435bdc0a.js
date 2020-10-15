70ecbf4910d92f382c807698bc1219ad0eabbfef
unknown <goodlucky1215@naver.com>
2020-10-16 00:11:22 +0900

    Updates

---
+++ precache-manifest.d5f6861748e8958e74dc08c0435bdc0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f04c0b42a0d7ca7fb831bf28d4d3223",
+    "url": "/nwitter.git/index.html"
+  },
+  {
+    "revision": "84e8b57c195f85f26888",
+    "url": "/nwitter.git/static/css/main.5953c0bb.chunk.css"
+  },
+  {
+    "revision": "bcc6f15c8440cbccddeb",
+    "url": "/nwitter.git/static/js/2.265b775a.chunk.js"
+  },
+  {
+    "revision": "7e320732ce8e269e8d356159c55ade0e",
+    "url": "/nwitter.git/static/js/2.265b775a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84e8b57c195f85f26888",
+    "url": "/nwitter.git/static/js/main.35ca4a92.chunk.js"
+  },
+  {
+    "revision": "b8d94dd6c70e98c49543",
+    "url": "/nwitter.git/static/js/runtime-main.2ba7d70b.js"
+  }
+]);
